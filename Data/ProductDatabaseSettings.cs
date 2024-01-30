e97526676fe382ad0085e6fa870afa37a8ab9bc6
esdrascosta <esdras.cts@outlook.com>
2024-01-30 15:42:46 -0300

    Configuracao inicial para o MongoDb

---
+++ Data/ProductDatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace CrudWithMongodb;
+
+public class ProductDatabaseSettings
+{
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
+    public string ProductCollectionName { get; set; } // As if it were the SQL table
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace CrudWithMongodb;
+
+public class ProductService
+{
+    private readonly IMongoCollection<Product> _productCollection;
+
+    public ProductService(IOptions<ProductDatabaseSettings> options)
+    {
+        var mongoClient = new MongoClient(options.Value.ConnectionString);
+        var mongoDatabase = mongoClient.GetDatabase(options.Value.DatabaseName);
+
+        _productCollection = mongoDatabase.GetCollection<Product>(options.Value.ProductCollectionName);
+    }
+
+}
